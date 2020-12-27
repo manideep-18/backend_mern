eb3d513ef41c2a45218ac7ce5164a49d4afa96bd
manideep-18 <r8manideep@gmail.com>
2020-12-27 12:48:53 +0530

    put functionality added for user model,route,controller

--- controllers/user.js
+++ controllers/user.js
@@ -18,3 +18,20 @@ exports.getUser=(req,res)=>{
     return res.json(req.profile)
 }
 
+exports.updateUser=(req,res)=>{
+    User.findByIdAndUpdate(
+        {_id:req.profile._id},
+        {$set:req.body},
+        {new:true,useFindAndModify:false},
+        (err,user)=>{
+            if(err){
+               return res.status(400).json({
+                    error:"You are not authorized"
+                })
+            }
+            user.salt=undefined
+            user.encry_password=undefined
+            res.json(user)
+        }
+    )
+}
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express=require('express')
 const { isSignedIn, isAuthenticated } = require('../controllers/auth')
-const { getUserById, getUser } = require('../controllers/user')
+const { getUserById, getUser, updateUser } = require('../controllers/user')
 const router=express.Router()
 
 router.param('userId',getUserById)
 
 router.get("/user/:userId",isSignedIn,isAuthenticated,getUser)
 
+router.put("/user/:userId",isSignedIn,isAuthenticated,updateUser)
 
 
 module.exports=router
