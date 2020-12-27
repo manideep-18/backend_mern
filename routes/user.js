9006edea1e66d572b8e056cfe546f733c1e3eeaa
manideep-18 <r8manideep@gmail.com>
2020-12-27 14:24:46 +0530

    pushOrderInPurchaseList middleware implemented in user controller

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 const express=require('express')
 const { isSignedIn, isAuthenticated } = require('../controllers/auth')
-const { getUserById, getUser, updateUser } = require('../controllers/user')
+const { getUserById, getUser, updateUser, userPurchaseList } = require('../controllers/user')
 const router=express.Router()
 
 router.param('userId',getUserById)
 
 router.get("/user/:userId",isSignedIn,isAuthenticated,getUser)
-
 router.put("/user/:userId",isSignedIn,isAuthenticated,updateUser)
+
+router.get("/orders/user/:userId",isSignedIn,isAuthenticated,userPurchaseList)
 
 
 module.exports=router
