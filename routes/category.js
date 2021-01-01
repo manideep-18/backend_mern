1c26d8f76ec2ed9d41013a0ea03851228d6772f3
manideep-18 <r8manideep@gmail.com>
2021-01-01 13:43:25 +0530

    params added for category section

--- routes/category.js
+++ routes/category.js
@@ -1,4 +1,10 @@
 const express= require('express')
 const router=express.Router()
 
+const { getCategoryById } = require('../controllers/category')
+const { getUserById } = require('../controllers/user')
+
+router.param("userId",getUserById)
+router.param("categoryId",getCategoryById)
+
 module.exports=router
