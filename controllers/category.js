4b0b9fbe44db7a5f50f8e4615a1ebff68d883d5c
manideep-18 <r8manideep@gmail.com>
2021-01-01 15:28:31 +0530

    category CRUD operations implemented

--- controllers/category.js
+++ controllers/category.js
@@ -12,3 +12,60 @@ exports.getCategoryById=(req,res,next,id)=>{
     })
 
 }
+
+exports.createCategory=(req,res)=>{
+    const category=new Category(req.body)
+    category.save((err,category)=>{
+        if(err){
+            return res.status(400).json({
+                error:"Not able to create category in DB"
+            })
+        }
+        res.json({category})
+    })
+}
+
+exports.getCategory=(req,res)=>{
+    res.json(req.category)
+}
+
+exports.getAllCategory=(req,res)=>{
+    Category.find().exec((err,categories)=>{
+        if(err){
+            return res.status(400).json({
+                error:"No Categories found in DB"
+            })
+
+        }
+        res.json(categories)
+    })
+}
+
+exports.updateCategory=(req,res)=>{
+    const category=req.category
+     category.name=req.body.name
+
+    category.save((err,updatedCategory)=>{
+        if(err){
+            return res.status(400).json({
+                error:"Failed to update category"
+            })
+        }
+        res.json(updatedCategory)
+    })
+}
+
+exports.removeCategory=(req,res)=>{
+     const category = req.category
+
+     category.remove((err,category)=>{
+        if(err){
+            return res.status(400).json({
+                error:"Failed to delete the category"
+            })
+        }
+        res.json({
+            message:"Succesfully Deleted"
+        })
+     })
+}
