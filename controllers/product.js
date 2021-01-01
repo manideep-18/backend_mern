bd35461c9e6d10312d48a02f026a252a0c39fabd
manideep-18 <r8manideep@gmail.com>
2021-01-01 17:53:56 +0530

    product CRUD operations implemented

--- controllers/product.js
+++ controllers/product.js
@@ -66,3 +66,89 @@ exports.createProduct=(req,res)=>{
 
     })
 }
+
+
+exports.getProduct=(req,res)=>{
+    req.product.photo=undefined
+    return res.json(req.product)
+}
+
+//middleware
+exports.photo = (req,res,next)=>{
+    if(req.product.photo.data){
+        res.set("Content-Type",req.product.photo.contentType)
+        return res.send(req.product.photo.data)
+    }
+    next()
+} 
+
+exports.deleteProduct=(req,res)=>{
+    let product=req.product
+    product.remove((err,deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                error:'Failed to delete Product'
+            })
+        }
+        return res.json({
+            message:'Deletion is success'
+        })
+    })
+}
+
+exports.updateProduct=(req,res)=>{
+    let form = new formidable.IncomingForm();
+    form.keepExtensions=true
+
+    form.parse(req,(err,fields,files)=>{
+        if(err){
+            return res.status(400).json({
+                error:'Problem with image'
+            })
+        }
+        
+        
+        let product = req.product;
+        product=_.extend(product,fields)
+
+        //handle file here
+        if(files.photo){
+            if(files.photo.size>3000000){
+                return res.status(400).json({
+                    error:'file size too big'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+        // save to the DB
+        product.save((err,product)=>{
+            if(err){
+            return res.status(400).json({
+                error:'Updation of product failed'
+            })
+        }
+        res.json(product)
+        })
+
+    })
+}
+
+exports.getAllProducts=(req,res)=>{
+    let limit = req.query.limit?parseInt(req.query.limit):8
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id" 
+
+    Product.find()
+    .select("-photo")
+    .populate("category")
+    .sort([[sortBy,"asc"]])
+    .limit(limit)
+    .exec((err,products)=>{
+        if(err){
+            res.status(400).json({
+                error:'No product found'
+            })
+        }
+        res.json(products)
+    })
+}
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth')
-const { getProductById, createProduct } = require('../controllers/product')
+const { getProductById, createProduct, getProduct, photo, updateProduct, deleteProduct, getAllProducts } = require('../controllers/product')
 const { getUserById } = require('../controllers/user')
 const router=express.Router()
 
@@ -8,5 +8,16 @@ router.param('userId',getUserById)
 router.param('productId',getProductById)
 
 router.post('/product/create/:userId',isSignedIn,isAuthenticated,isAdmin,createProduct)
+
+router.get('/product/:productId',getProduct)
+router.get('/product/photo/:productId',photo)
+
+router.delete("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,deleteProduct)
+
+router.put("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,updateProduct)
+
+//listing route
+router.get("/products",getAllProducts)
+
 
 module.exports=router
