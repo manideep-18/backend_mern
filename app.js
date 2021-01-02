6ced2c36eaa89a3911164bac59acafc58ed44357
manideep-18 <r8manideep@gmail.com>
2021-01-02 21:01:41 +0530

    order routes adedd

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const authRoutes=require("./routes/auth");
 const userRoutes=require("./routes/user");
 const categoryRoutes=require("./routes/category");
 const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+
 
 const app=express()
 
@@ -29,6 +31,7 @@ app.use("/api",authRoutes)
 app.use("/api",userRoutes)
 app.use("/api",categoryRoutes)
 app.use("/api",productRoutes)
+app.use("/api",orderRoutes)
 
 const port=process.env.PORT || 8000
 
